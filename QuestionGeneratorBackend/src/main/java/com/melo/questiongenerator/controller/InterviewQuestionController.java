179124d99018d9aa1bcf91bffb1e806b97affd42
PostgreSQL <postgres@Mac.lan>
2026-05-10 21:58:13 +0100

    Update CORS to allow github pages

--- QuestionGeneratorBackend/src/main/java/com/melo/questiongenerator/controller/InterviewQuestionController.java
+++ QuestionGeneratorBackend/src/main/java/com/melo/questiongenerator/controller/InterviewQuestionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173", "https://binawei.github.io"})
 public class InterviewQuestionController {
     private final GeminiService geminiService;
 
